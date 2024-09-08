a91b7861608651750cfa5a79dd7abf0085bd3a53
AnshBudhiraja7 <145669812+AnshBudhiraja7@users.noreply.github.com>
2024-09-08 23:29:53 +0530

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,11 @@ COPY ["package.json", "package-lock.json*", "./"]
 
 RUN npm install --production
 
-COPY . .
+COPY . .    
+
+ENV PARSE_APP_ID="9cdcb662-aaf2-4979-a92a-2dc854c62f7e"
+ENV PARSE_JS_KEY="12345"
+ENV PARSE_SERVER_URL="https://myapp-wk8rvkc8.b4a.run/"
 
 EXPOSE 3010
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Connection=require("./Connection")
 const schemas=require("./Schema")
 const express=require("express")
 const cors=require("cors")
+const Parse=require("parse/node")
 const dotenv=require("dotenv")
 dotenv.config()
 const app=express()
@@ -9,6 +10,9 @@ app.use(express.json())
 app.use(cors())
 Connection()
 
+
+Parse.initialize('9cdcb662-aaf2-4979-a92a-2dc854c62f7e', '12345');
+Parse.serverURL = 'https://myapp-wk8rvkc8.b4a.run/';
 app.post("/CreateUser",async(req,resp)=>{
     const {Name,Phone}=req.body
     if(Name && Phone)
@@ -19,12 +23,8 @@ app.post("/CreateUser",async(req,resp)=>{
     return resp.status(404).send({Message:"Field is Empty"})  
 })
 app.get("/ShowUsers",async(req,resp)=>{
-    try{
-    const result=await schemas.find({})
+    const result=await schemas.find()
     return resp.status(201).send({result,Message:"Fetched Successfully"})
-    }catch(error){
-    return resp.status(500).send({error,Message:"Internal Server Error"})
-    }
 })
 app.delete("/DeleteUser",async(req,resp)=>{
     const result=await schemas.deleteOne({_id:req.body.id})
@@ -35,6 +35,5 @@ app.put("/UpdateUser",async(req,resp)=>{
     return resp.status(201).send({result,Message:"Updated Successfully"})  
 })
 app.listen(process.env.PORT || 3010,()=>{
-    console.log("Server Started At:"+process.env.PORT || 3010);
-    
+    console.log("Server Started At:"+process.env.PORT || 3010);  
 })
