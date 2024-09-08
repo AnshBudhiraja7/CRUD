702012a7c2d24c4c937fe24ddb9acc4650d79c3c
AnshBudhiraja7 <145669812+AnshBudhiraja7@users.noreply.github.com>
2024-09-09 01:03:53 +0530

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,6 @@ RUN npm install --production
 
 COPY . .    
 
-ENV PARSE_APP_ID="9cdcb662-aaf2-4979-a92a-2dc854c62f7e"
-ENV PARSE_JS_KEY="12345"
-ENV PARSE_SERVER_URL="https://myapp-wk8rvkc8.b4a.run/"
-
 EXPOSE 3010
 
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Connection=require("./Connection")
 const schemas=require("./Schema")
 const express=require("express")
 const cors=require("cors")
-const Parse=require("parse/node")
 const dotenv=require("dotenv")
 dotenv.config()
 const app=express()
@@ -10,29 +9,42 @@ app.use(express.json())
 app.use(cors())
 Connection()
 
-
-Parse.initialize('9cdcb662-aaf2-4979-a92a-2dc854c62f7e', '12345');
-Parse.serverURL = 'https://myapp-wk8rvkc8.b4a.run/';
 app.post("/CreateUser",async(req,resp)=>{
+    try {
     const {Name,Phone}=req.body
     if(Name && Phone)
     {
         const result=await schemas.create({Name,Phone})
        return resp.status(201).send({result,Message:"Saved Successfully"})  
     }
-    return resp.status(404).send({Message:"Field is Empty"})  
+    return resp.status(404).send({Message:"Field is Empty"})
+    } catch (error) {
+    return resp.status(500).send({Message:"Internal Server Error"})  
+    }  
 })
 app.get("/ShowUsers",async(req,resp)=>{
+    try {
     const result=await schemas.find()
     return resp.status(201).send({result,Message:"Fetched Successfully"})
+    } catch (error) {
+    return resp.status(500).send({Message:"Internal Server Error"})     
+    }
 })
 app.delete("/DeleteUser",async(req,resp)=>{
+    try {
     const result=await schemas.deleteOne({_id:req.body.id})
-    return resp.status(201).send({result,Message:"Deleted Successfully"})  
+    return resp.status(201).send({result,Message:"Deleted Successfully"})
+    } catch (error) {
+    return resp.status(500).send({Message:"Internal Server Error"})     
+    }  
 })
 app.put("/UpdateUser",async(req,resp)=>{
+   try {
     const result=await schemas.updateOne({_id:req.body.id},{$set:req.body.data})
     return resp.status(201).send({result,Message:"Updated Successfully"})  
+   } catch (error) {
+    return resp.status(500).send({Message:"Internal Server Error"})  
+   }
 })
 app.listen(process.env.PORT || 3010,()=>{
     console.log("Server Started At:"+process.env.PORT || 3010);  
